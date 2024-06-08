3d094f27b73d27a65516714e89d72ee3a6d3be4c
ForgeDom <maxstill2006@gmail.com>
2024-06-08 17:15:12 +0300

    +

--- main.cpp
+++ main.cpp
@@ -2,43 +2,49 @@
 #include <ctime>
 using namespace std;
 
-
-int* createArray(int* size) {
-    int* arr = new int[*size];
-    for (int i = 0; i < *size; i++) {
-        arr[i] = rand() % 255;
+int* createArr(int* arr1, int* size) {
+    for (int i = 0; i < *size; ++i) {
+        arr1[i] = rand() % 100;
     }
-    return arr;
+    return arr1;
 }
 
-
-void printArray(int* arr, int* size) {
-    for (int i = 0; i < *size; i++) {
-        cout << *(arr + i) << " ";
+void printArr(int* arr1, int* size) {
+    for(int i = 0; i < *size; i++) {
+        cout << *(arr1+i) << " ";
     }
     cout << endl;
 }
 
-int* sumArr(int* arr ,int* size) {
-    int* sum = new int{ 0 };
-    for (int i = 0; i < *size; ++i) {
-        *sum += arr[i];
+int* exchangeArrs(int* arr1, int* arr2, int* size) {
+    for (int i = 0; i < *size; i++) {
+        *(arr2 + i) = *(arr1 + i);
     }
-    return sum;
+    return arr2;
 }
 
 int main() {
     srand(time(0));
-
     int* size = new int{ 5 };
-    int* arr = createArray(size);
-    printArray(arr, size);
-    int* result = sumArr(arr, size);
-    cout << *result << endl;
 
-    delete[] arr;
+    int* arr1 = new int[*size]; 
+    createArr(arr1, size);
+    printArr(arr1, size);
+
+    int* arr2 = new int[*size];
+    createArr(arr2, size);
+    printArr(arr2, size);
+
+    exchangeArrs(arr1, arr2, size);
+
+    cout << "Array 1 after exchange: ";
+    printArr(arr1, size);
+    cout << "Array 2 after exchange: ";
+    printArr(arr2, size);
+
+    delete[] arr1;
+    delete[] arr2;
     delete size;
-    delete result;
     return 0;
 }
 
