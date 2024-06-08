7903f0134b2bbb01c5bc5edd7768769398e524c9
ForgeDom <maxstill2006@gmail.com>
2024-06-08 16:10:40 +0300

    +

--- main.cpp
+++ main.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main() {
+	int* num = new int;
+	cout << "Enter number:" << endl;
+	cin >> *num;
 
+	if (*num > 0) {
+		cout << "Number has positive sign" << endl;
+	}
+	else if (*num < 0) {
+		cout << "Number has negative sign" << endl;
+	}
+	else {
+		cout << "Number equals zero" << endl;
+	}
 	return 0;
 }
