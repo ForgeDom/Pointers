223b56d1c344290fab54334066f34a0a7c00301b
ForgeDom <maxstill2006@gmail.com>
2024-06-08 16:36:19 +0300

    +

--- main.cpp
+++ main.cpp
@@ -2,7 +2,68 @@
 #include <ctime>
 using namespace std;
 
-int main() {
 
-	return 0;
+int sum(int* a, int* b) {
+    return *a + *b;
 }
+
+int subtract(int* a, int* b) {
+    return *a - *b;
+}
+
+int multiply(int* a, int* b) {
+    return *a * *b;
+}
+
+double divide(int* a, int* b) {
+    return static_cast<double>(*a) / *b;
+}
+
+int main() {
+    int* a = new int;
+    int* b = new int;
+    int* choice = new int;
+
+    while (true) {
+        cout << "Enter first value:" << endl;
+        cin >> *a;
+        cout << "Enter second value:" << endl;
+        cin >> *b;
+
+        cout << "Choose operation:" << endl;
+        cout << "1 - (+)\n2 - (-)\n3 - (*)\n4 - (/)\n5 - Exit" << endl;
+        cin >> *choice;
+
+
+
+        switch (*choice) {
+            case 1:
+                cout << sum(a, b) << endl;
+                break;
+            case 2:
+                cout << subtract(a, b) << endl;
+                break;
+            case 3:
+                cout << multiply(a, b) << endl;
+                break;
+            case 4:
+                if (*b != 0) {
+                    cout << divide(a, b) << endl;
+                }
+                else {
+                    cout << "Cant divide by zero" << endl;
+                }
+                break;
+            case 5:
+                return 0;
+        }
+    }
+
+    delete a;
+    delete b;
+    delete choice;
+
+    return 0;
+}
+
+
