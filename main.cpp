e594f5342e889dd2d242a3c9c277fee6f98593a6
ForgeDom <maxstill2006@gmail.com>
2024-06-08 16:15:20 +0300

    +

--- main.cpp
+++ main.cpp
@@ -3,6 +3,22 @@
 using namespace std;
 
 int main() {
+	int* num1 = new int;
+	cout << "Enter first number:" << endl;
+	cin >> *num1;
+	int* num2 = new int;
+	cout << "Enter second number:" << endl;
+	cin >> *num2;
 
+	cout << *num1 << " - " << *num2 << endl;
+
+	int temp = *num1;
+	*num1 = *num2;
+	*num2 = temp;
+
+	cout << *num1 << " - " << *num2 << endl;
+
+	delete num1;
+	delete num2;
 	return 0;
 }
