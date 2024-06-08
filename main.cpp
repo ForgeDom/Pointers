c639b2d2ebe94f0c87da94939ace658761d0932f
ForgeDom <maxstill2006@gmail.com>
2024-06-08 16:08:19 +0300

    +

--- main.cpp
+++ main.cpp
@@ -3,6 +3,21 @@
 using namespace std;
 
 int main() {
-
+	int* num1 = new int;
+	cout << "Enter first number:" << endl;
+	cin >> *num1;
+	int* num2 = new int;
+	cout << "Enter second number:" << endl;
+	cin >> *num2;
+	
+	if (*num1 > *num2) {
+		cout << "Number 1 is bigger" << endl;
+	}
+	else if(*num1 < *num2){
+		cout << "Number 2 is bigger" << endl;
+	} 
+	else {
+		cout << "Numbers are equal" << endl;
+	}
 	return 0;
 }
